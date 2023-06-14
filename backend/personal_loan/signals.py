ce6fb3bcd4cd28468e5ddc2a2fc64bc4c1c605d3
Emows_Lemos <emows10@hotmail.com>
2023-06-14 01:57:39 -0300

    minor fix

--- backend/personal_loan/signals.py
+++ backend/personal_loan/signals.py
@@ -3,13 +3,9 @@ from django.dispatch import receiver
 from personal_loan.tasks import loan_proposal_status_task
 from personal_loan.models import Loan
 
-
 @receiver(post_save, sender=Loan)
 def evaluate_loan_status(sender, instance, created, **kwargs):
-    if created:
-        approved_loans = Loan.objects.filter(proposal_status="approved").count()
-        rejected_loans = Loan.objects.filter(proposal_status="rejected").count()
-        loan_proposal_status_task.delay(instance.id, approved_loans, rejected_loans)
-
+      if created:
+            loan_proposal_status_task.delay(instance.id)
 
 post_save.connect(evaluate_loan_status, sender=Loan)
